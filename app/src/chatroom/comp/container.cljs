b4249214151b359b1f532d65bc7d0c4600696242
jiyinyiyong <jiyinyiyong@gmail.com>
2016-12-08 00:55:36 +0800

    refactor router

--- app/src/chatroom/comp/container.cljs
+++ app/src/chatroom/comp/container.cljs
@@ -6,19 +6,29 @@
             [respo.alias :refer [create-comp div span]]
             [respo.comp.debug :refer [comp-debug]]
             [respo.comp.text :refer [comp-code comp-text]]
-            [chatroom.comp.home :refer [comp-home]]
             [chatroom.comp.header :refer [comp-header]]
+            [chatroom.comp.profile :refer [comp-profile]]
+            [chatroom.comp.topics :refer [comp-topics]]
+            [chatroom.comp.room :refer [comp-room]]
+            [chatroom.comp.login :refer [comp-login]]
             [respo-message.comp.msg-list :refer [comp-msg-list]]))
 
 (def style-body {})
 
 (defn render [store]
   (fn [state mutate!]
-    (div
-     {:style (merge ui/global ui/fullscreen ui/column)}
-     (comp-header (:logged-in? store))
-     (div {:style style-body} (comp-home store))
-     (comp-debug (:logged-in? store) {:bottom 0, :max-width "100%", :left 0})
-     (comp-msg-list (get-in store [:state :notifications]) :state/remove-notification))))
+    (let [router (get-in store [:state :router])]
+      (div
+       {:style (merge ui/global ui/fullscreen ui/column)}
+       (comp-header (:logged-in? store))
+       (if (:logged-in? store)
+         (case (:name router)
+           :home (comp-topics (:topics store) (:logged-in? store))
+           :topic (comp-room (:seeing-messages store) (:data router))
+           :profile (comp-profile store)
+           nil)
+         (comp-login))
+       (comp-debug router {:bottom 0, :max-width "100%", :left 0})
+       (comp-msg-list (get-in store [:state :notifications]) :state/remove-notification)))))
 
 (def comp-container (create-comp :container render))
--- app/src/chatroom/comp/header.cljs
+++ app/src/chatroom/comp/header.cljs
@@ -7,6 +7,9 @@
             [respo.comp.debug :refer [comp-debug]]
             [respo.comp.text :refer [comp-code comp-text]]))
 
+(defn on-profile [e dispatch!]
+  (dispatch! :router/change {:router nil, :name :profile, :data nil}))
+
 (def style-header
   {:color :white,
    :font-size 16,
@@ -15,13 +18,18 @@
    :justify-content :space-between,
    :height 48})
 
+(defn on-home [e dispatch!]
+  (dispatch! :router/change {:router nil, :name :home, :data nil}))
+
+(def style-cursor {:cursor "pointer"})
+
 (defn render [logged-in?]
   (fn [state mutate!]
     (div
      {:style (merge ui/row-center style-header)}
-     (div {} (comp-text "Messages" nil))
+     (div {:style style-cursor, :event {:click on-home}} (comp-text "Messages" nil))
      (div
-      {:style {:cursor "pointer"}, :event {}}
+      {:style style-cursor, :event {:click on-profile}}
       (comp-text (if logged-in? "Me" "Guest") nil)))))
 
 (def comp-header (create-comp :header render))
